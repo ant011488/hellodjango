7cfa3085df14e96642397f460c4cf43a8e94228b
Anthony Heyliger <anthony.heyliger@gmail.com>
2014-06-15 23:16:04 +0000

    second commit

--- hellodjango/wsgi.py
+++ hellodjango/wsgi.py
@@ -7,8 +7,6 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
-import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hellodjango.settings")
 
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
