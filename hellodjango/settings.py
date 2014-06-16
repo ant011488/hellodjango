a9236a0b402147189c37f28eaeb84fb3ed3b8327
Anthony Heyliger <anthony.heyliger@gmail.com>
2014-06-16 01:05:20 +0000

    6th commit

--- hellodjango/settings.py
+++ hellodjango/settings.py
@@ -54,8 +54,8 @@ WSGI_APPLICATION = 'hellodjango.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.',
+        'NAME': os.path.join(BASE_DIR, ''),
     }
 }
 
